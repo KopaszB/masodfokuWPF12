502fbe12fa642c33aecb5f22f034f19b7c750f91
Béla Kopasz <kopaszbeci@gmail.com>
2026-03-31 10:11:31 +0200

    kész

--- masodfokuWPF/MainWindow.xaml.cs
+++ masodfokuWPF/MainWindow.xaml.cs
@@ -24,5 +24,46 @@ namespace masodfokuWPF
         {
             InitializeComponent();
         }
+
+        private void btn_szamol_Click(object sender, RoutedEventArgs e)
+        {
+            if (!szamE(tbx_a.Text)|| !szamE(tbx_b.Text)|| !szamE(tbx_c.Text))
+            {
+                MessageBox.Show("Nem számot adtál meg!", "Hiba");
+                return;
+            }
+            double a = int.Parse(tbx_a.Text);
+            double b = int.Parse(tbx_b.Text);
+            double c = int.Parse(tbx_c.Text);
+
+            double disk = b * b - 4 * a * c;
+            if (disk>0)
+            {
+                double x1 = (-b + Math.Sqrt(disk)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(disk)) / (2 * a);
+                tbl_eredmeny.Text = $"Két valós gyök: x1={x1:f2}, x2={x2:f2}";
+            }
+            else if (disk<0)
+            {
+                tbl_eredmeny.Text = $"Nincs megoldás, mert a diszkrimináns negatív!";
+            }
+            else
+            {
+                double x = -b / (2 * a);
+                tbl_eredmeny.Text = $"Egy valós gyök: x={x:f2}";
+            }
+        }
+
+        private bool szamE(string szam)
+        {
+            if (double.TryParse(szam, out double szamSZ))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
